a816f15882ca0fdda39f7bae7a971bd1923e3550
ChaoticGabibo@gmail.com <ChaoticGabibo@gmail.com@0c664daf-bfc8-a7be-0b75-2a438262636e>
2011-05-19 01:17:49 +0000

    Cartridge does not have anything more than a 32 kB memory... Next step is to integrate it to JavaBoy...

--- src/Cartridge.java
+++ src/Cartridge.java
@@ -21,60 +21,17 @@ Place - Suite 330, Boston, MA 02111-1307, USA.
 
 */
 
-import java.awt.*;
+//import java.awt.*;
 import java.io.*;
 
-/** This class represents the game cartridge and contains methods to load the ROM and battery RAM
- *  (if necessary) from disk or over the web, and handles emulation of ROM mappers and RAM banking.
- *  It is missing emulation of MBC3 (this is very rare).
- */
-
 class Cartridge {
-	/** Translation between ROM size byte contained in the ROM header, and the number
-	 *  of 16Kb ROM banks the cartridge will contain
-	 */
-	final int[][] romSizeTable = {{0, 2}, {1, 4}, {2, 8}, {3, 16}, {4, 32},
-	                              {5, 64}, {6, 128}, {7, 256}, {0x52, 72}, {0x53, 80}, {0x54, 96}};
 
 	/** Contains the complete ROM image of the cartridge */
-	public byte[] rom;
-
-	/** Number of 16Kb ROM banks */
-	int numBanks = 2;
-
-	/** Cartridge type - index into cartTypeTable[][] */
-	//int cartType = 0;
-
-	/** Starting address of the ROM bank at 0x4000 in CPU address space */
-	int pageStart = 0x4000;
-
-	/** The bank number which is currently mapped at 0x4000 in CPU address space */
-	int currentBank = 1;
-
-	/** The bank which has been saved when the debugger changes the ROM mapping.  The mapping is
-	 *  restored from this register when execution resumes */
-	int savedBank = -1;
-
-	/** The RAM bank number which is currently mapped at 0xA000 in CPU address space */
-	int ramBank;
-	int ramPageStart;
-
-	boolean mbc1LargeRamMode = false;
-	boolean ramEnabled = false;
-	Component applet;
-
-	boolean needsReset = false;
-
-	/** Real time clock registers.  Only used on MBC3 */
-	//int[] RTCReg = new int[5];
-	//long realTimeStart;
-	//long lastSecondIncrement;
-	String romIntFileName;
+	public byte[] rom = new byte[0x8000];
 
 	/** Create a cartridge object, loading ROM and any associated battery RAM from the cartridge
 	 *  filename given.  Loads via the web if JavaBoy is running as an applet */
 	public Cartridge() {
-		rom = new byte[0x04000 * numBanks];   // Recreate the ROM array with the correct size
 		try {
 			InputStream is = new FileInputStream(new File("../roms/rom.gb"));
 			is.read(rom); // Read the entire ROM
@@ -84,49 +41,17 @@ class Cartridge {
 		} catch (IndexOutOfBoundsException e) {
 			System.out.print("Error, Loading the ROM image failed.The file is not a valid Gameboy ROM.");
 		}
-
 	}
 
 	/** Returns the byte currently mapped to a CPU address.  Addr must be in the range 0x0000 - 0x4000 or
 	 *  0xA000 - 0xB000 (for RAM access)
 	 */
 	public final byte addressRead(int addr) {
-		if (addr < 0x4000) {
-			return (byte) (rom[addr]);
-		} else {
-			return (byte) (rom[pageStart + addr - 0x4000]);
-		}
+		return (byte) (rom[addr]);
 	}
 
 	/** Performs saving of the battery RAM before the object is discarded */
 	public void dispose() {
-	}
-
-	public boolean verifyChecksum() {
-		int checkSum = (JavaBoy.unsign(rom[0x14E]) << 8) + JavaBoy.unsign(rom[0x14F]);
-
-		int total = 0;                   // Calculate ROM checksum
-		for (int r=0; r < rom.length; r++) {
-			if ((r != 0x14E) && (r != 0x14F)) {
-				total = (total + JavaBoy.unsign(rom[r])) & 0x0000FFFF;
-			}
-		}
-
-		return checkSum == total;
-	}
-
-	/** Returns the number of 16Kb banks in a cartridge from the header size byte. */
-	public int lookUpCartSize(int sizeByte) {
-		int i = 0;
-		while ((i < romSizeTable.length) && (romSizeTable[i][0] != sizeByte)) {
-			i++;
-		}
-
-		if (romSizeTable[i][0] == sizeByte) {
-			return romSizeTable[i][1];
-		} else {
-			return -1;
-		}
 	}
 
 }
